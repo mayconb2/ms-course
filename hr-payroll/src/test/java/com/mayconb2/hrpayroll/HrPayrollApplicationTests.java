301d5283a1a2bb1ce4b72593aa79c98bf2965ad7
Maycon de Souza <mayconb2@gmail.com>
2021-03-29 23:46:21 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/mayconb2/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayconb2.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
