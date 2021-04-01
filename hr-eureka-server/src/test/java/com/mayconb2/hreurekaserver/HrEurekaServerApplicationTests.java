ce78c87d6b79ce7c6ce5664b50fcc5d87da699e0
Maycon de Souza <mayconb2@gmail.com>
2021-03-31 23:17:35 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/mayconb2/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayconb2.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
