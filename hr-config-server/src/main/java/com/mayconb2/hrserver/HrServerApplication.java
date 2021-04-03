3dbf970c90a80617fb6f479ca924dd4bb701075b
Maycon de Souza <mayconb2@gmail.com>
2021-04-03 00:38:22 -0300

    Project hr-config-server created

---
+++ hr-config-server/src/main/java/com/mayconb2/hrserver/HrServerApplication.java
@@ -0,0 +1,13 @@
+package com.mayconb2.hrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrServerApplication.class, args);
+    }
+
+}
