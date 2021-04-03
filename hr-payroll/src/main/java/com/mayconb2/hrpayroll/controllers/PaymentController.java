2a85f4f0c3f24db7d5ec27afd3ef16530b192b05
Maycon de Souza <mayconb2@gmail.com>
2021-04-02 23:53:29 -0300

    Hystrix fault tolerance with mock

--- hr-payroll/src/main/java/com/mayconb2/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/mayconb2/hrpayroll/controllers/PaymentController.java
@@ -2,6 +2,7 @@ package com.mayconb2.hrpayroll.controllers;
 
 import com.mayconb2.hrpayroll.entities.Payment;
 import com.mayconb2.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +21,16 @@ public class PaymentController {
         this.service = service;
     }
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = new Payment("Maycon",200.0,1);
+        return ResponseEntity.ok(payment);
+    }
+
 }
